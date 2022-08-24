0b1dffbaeaaafc3221917ccf40b0b35eb33f761b
VladyslavPK <vladiyslav.polienkov@gmail.com>
2022-08-25 00:16:02 +0300

    deploy react todo app

---
+++ precache-manifest.d00bf293665089295e309fef01afa084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20490eba428099d5e100",
+    "url": "/react-todo-app/static/css/main.46c13ac4.chunk.css"
+  },
+  {
+    "revision": "20490eba428099d5e100",
+    "url": "/react-todo-app/static/js/main.20490eba.chunk.js"
+  },
+  {
+    "revision": "3dd370925ab85f2a7620",
+    "url": "/react-todo-app/static/js/1.3dd37092.chunk.js"
+  },
+  {
+    "revision": "558acc1cfe7907a3db4e",
+    "url": "/react-todo-app/static/js/2.558acc1c.chunk.js"
+  },
+  {
+    "revision": "8a7144157fec1448c79c",
+    "url": "/react-todo-app/static/js/runtime~main.8a714415.js"
+  },
+  {
+    "revision": "46f50f0faf4f5d14b80193614a10c94d",
+    "url": "/react-todo-app/index.html"
+  }
+];
